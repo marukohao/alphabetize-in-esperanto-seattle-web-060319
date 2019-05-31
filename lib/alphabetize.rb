d034fbdbc290e0b727fc5298ca30816d01da9053
hao wang <ge_nah@hotmail.com>
2019-05-31 00:40:04 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
+  string = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   # code here
+  arr[0].each_char.with_index do |char, i|
+    if !arr.all? {|word| word[i] != char}
+      arr.each.sort_by {|word| string.index(word[i])}
+    end
+  end
 end
